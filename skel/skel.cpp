45bedea6478355142a06869fa5a23c06b14f4cac
SergeyVostokin <easts@mail.ru>
2016-10-12 21:52:59 +0400

    new skeleton processor

--- skel/skel.cpp
+++ skel/skel.cpp
@@ -1,241 +1,214 @@
-// Copyright 2016 Vadim Mishenev, Sergei Vostokin
+/*--------------------------------------------------------------------------*/
+/*  Copyright 2016 Sergei Vostokin                                          */
+/*                                                                          */
+/*  Licensed under the Apache License, Version 2.0 (the "License");         */
+/*  you may not use this file except in compliance with the License.        */
+/*  You may obtain a copy of the License at                                 */
+/*                                                                          */
+/*  http://www.apache.org/licenses/LICENSE-2.0                              */
+/*                                                                          */
+/*  Unless required by applicable law or agreed to in writing, software     */
+/*  distributed under the License is distributed on an "AS IS" BASIS,       */
+/*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*/
+/*  See the License for the specific language governing permissions and     */
+/*  limitations under the License.                                          */
+/*--------------------------------------------------------------------------*/
 
+#include <string>
+#include <list>
+#include <map>
 #include <iostream>
-#include "skel.h"
+#include <fstream>
+using namespace std;
 
-void Analyzator::analyze() {
-	infile.open(in, std::ios_base::in);//режим in
-	if (!infile) {//открыть файл не удалось
-		std::cerr << "Ќе могу открыть\"" << in << "\"дл€ чтени€\n";
-		//exit( -1 );
-		return;
+enum LTYPE{ FMARK, LMARK, TEXT, FAULTY };
+
+const string lead_sign_beg = "/*$TET$";
+const string lead_sign_end = "*/";
+const string close_sign = "/*$TET$*/";
+
+const char DELIM[] = " \t";
+
+typedef list<string> Block;
+
+LTYPE linetype(string& line, string& key)
+{
+	size_t pos;
+	if ((pos = line.find(close_sign)) != string::npos){
+		// /*tet*/
+		size_t ptoken = line.find_first_not_of(DELIM);
+		if (ptoken != pos) return TEXT;
+
+		size_t after_close_sign_pos = pos + close_sign.size();
+		if (line.find_first_not_of(DELIM, after_close_sign_pos) != string::npos) return FAULTY;
+
+		return LMARK;
 	}
-	State state = startHead;
-	char ch; int i = 0;
-	std::string buf, buffinder;
-	while (infile.get(ch)) {
-		//std
-		//ищем начало
-		if (state == startHead) {
-			if (ch == startHeaderText[i]) {
-				if (i == startHeaderText.length() - 1) {
-					state = endHead;
-					i = 0;
-				}
-				else
-					i++;
-				continue;
-			} else if (i > 0) skeletonfile.seekg((int)infile.tellg() - i);
-		}
-		//ищем конец  заголовка
-        if (state == endHead ) {
-		//	buf+=ch;
-			if (ch == endHeaderText[i] /*|| (ch == endHeaderText[0] && (i=0))*/) {
-				if (i == endHeaderText.length() - 1) {
-					state = endBlock;
-					buf.clear(); buffinder.clear();
-					i = 0;
-				} else {
-					//buffinder += ch;
-					i++;
-				}
-				continue;
-			}	else if (i > 0) {
+	else if ((pos = line.find(lead_sign_beg)) != string::npos){
+		// /*tet$key*/
+		size_t ptoken = line.find_first_not_of(DELIM);
+		if (ptoken != pos) return TEXT;
 
-					key += endHeaderText[0];
-					infile.seekg((int)infile.tellg() - i);
-				/*	for (int j = 1, ji=0; j < buffinder.length; j++) {
-						if (buffinder[j] == endHeaderText[ji]) {
+		size_t key_pos = pos + lead_sign_beg.size();
+		size_t after_key_pos = line.find(lead_sign_end, key_pos);
 
-						}
-					}*/
-				} else {
-					key += ch;
+		if (after_key_pos == string::npos) return FAULTY;
+
+		size_t after_mark_pos = after_key_pos + lead_sign_end.size();
+
+		if (line.find_first_not_of(DELIM, after_mark_pos) != string::npos) return FAULTY;
+
+		key = line.substr(key_pos, after_key_pos - key_pos);
+		return FMARK;
+	}
+	else return TEXT;
+}
+
+int main(int argc, char* argv[])
+{
+	bool isRelease = false, helpme = false, place_bottom = false;
+	string infile, outfile, skeleton;
+
+	for (int i = 1; i < argc; i++) {
+		char *cmd = argv[i];
+
+		if (cmd[0] == '-' || cmd[0] == '/') {
+			switch (cmd[1]) {
+			case 'O':case 'o':
+				outfile = string(argv[i + 1]);
+				break;
+			case 'I':case 'i':
+				infile = string(argv[i + 1]);
+				break;
+			case 'S':case 's':
+				skeleton = string(argv[i + 1]);
+				break;
+			case 'R':case 'r':
+				isRelease = true;
+				break;
+			case 'H':case 'h':
+				helpme = true;
+				break;
+			case 'B':case 'b':
+				place_bottom = true;
+				break;
 			}
+		}
+	}
+
+	if (helpme || argc==1){
+		cout << "Templet skeleton processor. Copyright Sergei Vostokin, 2016" << endl
+			<< "Usage:  skel <options>\n" << endl
+			<< " -s -S <skeleton file name>" << endl
+			<< " -i -I <input file name>" << endl
+			<< " -o -O <output file name> -- assumed to be the same as an input, if not defined" << endl
+			<< " -r -R -- use no markup in realease version of the output file" << endl
+			<< " -b -B -- place blocks that have no pair in the skeleton into the bottom of output file" << endl
+			<< " -h -H -- print this screen" << endl;
+
+	}
+
+	if (outfile.empty()){ outfile = infile; place_bottom = true; }
+	if (infile.empty() || skeleton.empty())	{
+		cout << "ERROR: bad param(s): input and skeleton files must be defined\n";
+		return 1;
+	}
+
+	map<string, Block> block_map;
+
+	{
+		ifstream file(infile);
+
+		int count = 0;
+		bool read_block = false;
+		string key;
+		Block block;
+
+		if (!file){ cout << "ERROR: could not open input file '" << infile << "'"; return 1; }
+		
+		string line;
+		getline(file, line); count++;
+		
+		while (file){
+			switch (linetype(line, key)){
+			case FMARK:
+				read_block = true;
+				break;
+			case LMARK:
+				read_block = false;
+				block_map[key] = block;
+				block.clear();
+				break;
+			case TEXT:
+				if (read_block)block.push_back(line);
+				break;
+			case FAULTY: cout << "ERROR: a faulty block found in input file '"<< infile<<"' at line #"<<count; return 1;
+			}
+			getline(file, line); count++;
+		}
+	}
+
+	{
+		string key;
+		
+		bool read_block = false;
+		bool have_substituted_block=false;
+
+		int count = 0;
+		ifstream ifile(skeleton);
+		ofstream ofile(outfile);
+
+		if (!ifile){ cout << "ERROR: could not open skeleton file '" <<skeleton<< "for reading"; return 1; }
+		if (!ofile){ cout << "ERROR: could not open output file '" << outfile << "for writing"; return 1; }
+
+		string line;
+		getline(ifile, line); count++;
+
+		while (ifile){
+			map<string, Block>::iterator it;
 			
-		}
-		//ищем конец  блока
-		if (state == endBlock) {
-			buf += ch;
-			if (ch == endBlockText[i] || (ch == endBlockText[0] && (i = 0) )) {
-				if (i == endBlockText.length() - 1) {
-					value = buf.substr(0, buf.length() - endBlockText.length());
-#ifdef _DEBUG
-					std::cout << key << " | "<<value << std::endl; 
-#endif
-
-					store.insert(std::pair<std::string, std::string>(key, value));
-					key.clear(); value.clear();
-					buf.clear();
-					state = startHead;
-					i = 0;
-				}
-				else
-					i++;
-				continue;
-			}
-		}
-
-		i = 0;
-	}
-	infile.close();
-}
-void Analyzator::write() {
-	
-	skeletonfile.open(skeleton, std::ios_base::in);//режим in
-	if (!skeletonfile) {//открыть файл не удалось
-		std::cerr << "Ќе могу открыть\"" << skeleton << "\"дл€ чтени€\n";
-		//exit( -1 );
-		return;
-	}
-
-	outfile.open(out, std::ios_base::out);//режим out
-	if (!outfile) {//открыть файл не удалось
-		std::cerr << "Ќе могу открыть\"" << out << "\"дл€ записи\n";
-		//exit( -1 );
-		return;
-	}
-
-	State state = startHead;
-	char ch; int i = 0;
-	while (skeletonfile  .get(ch)) {
-		//ищем начало
-		if (state == startHead ) {	
-			if (ch == startHeaderText[i]) {
-				if (i == startHeaderText.length() - 1) {
-					state = endHead;
-					if (!isRealese) outfile << startHeaderText;
-					i = 0;
-				}
-				else 
-					i++;
-				continue;
+			switch (linetype(line, key)){
+			case FMARK:
+				read_block = true;
+				if(!isRelease) ofile << lead_sign_beg << key << lead_sign_end << endl;
 				
-			} else if (i > 0) {
-				outfile << endHeaderText[0];
-				skeletonfile.seekg((int)infile.tellg() - i);
-			}
-			else {
-				outfile << ch;
-			}
-		}
-		//ищем конец  заголовка
-		if (state == endHead) {
-			//	buf+=ch;
-			if (ch == endHeaderText[i] /*|| (ch == endHeaderText[0] && (i=0))*/) {
-				if (i == endHeaderText.length() - 1) {
-					if (!isRealese) outfile << key<< endHeaderText;
-					
-					state = endBlock;
-					i = 0;
-				}
-				else 
-					i++;;
-				continue;
-			}
-			else if (i > 0) {
-				key+= endHeaderText[0];
-				skeletonfile.seekg((int)infile.tellg() - i);		
-			}else {
-				key += ch;
-			}
-
-		}
-		//ищем конец  блока
-		if (state == endBlock) {
-			if (ch == endBlockText[i] || (ch == endHeaderText[0] && (i = 0))) {
-				if (i == endBlockText.length() - 1) {
-					std::map<std::string, std::string>::iterator it = store.find(key);
-					if (it != store.end()){
-#ifdef _DEBUG
-						std::cout << key << " | " << it->second;
-#endif
-						outfile << it->second;
+				it=block_map.find(key);
+				if (it != block_map.end()){
+					have_substituted_block = true;
+					list<string>::const_iterator its;
+					for (its = (*it).second.begin(); its != (*it).second.end(); its++){
+						ofile << (*its) << endl;
 					}
-					else {
-						std::cout << "Ќеизвестный ключ " << key << "\n";
-					}
-					if (!isRealese) outfile << endBlockText;
-					key.clear();
-					state = startHead;
-					i = 0;
+					block_map.erase(it);
+				}else
+					have_substituted_block = false;
+				break;
+			case LMARK:
+				read_block = false;
+				if (!isRelease) ofile << close_sign << endl;
+				break;
+			case TEXT:
+				if (!read_block || (read_block && !have_substituted_block)) ofile << line << endl;
+				break;
+			case FAULTY: cout << "ERROR: a faulty block found in skeleton file '" << skeleton << "' at line #" << count; return 1;
+			}
+			getline(ifile, line); count++;
+		}
+
+		if (!block_map.empty()){
+			cout << "WARNING: not all blocks from the input file '" << infile << "' have a pair in the skeleton file '" << skeleton <<"'" << endl;
+			if (place_bottom){
+				map<string, Block>::const_iterator it;
+				for (it = block_map.begin(); it != block_map.end(); it++){
+					ofile << lead_sign_beg << (*it).first << lead_sign_end << endl;
+					list<string>::const_iterator its;
+					for (its = (*it).second.begin(); its != (*it).second.end(); its++)
+						cout << (*its) << endl;
+					ofile << close_sign << endl;
 				}
-				else
-					i++;
-				continue;
 			}
 		}
-
-		i = 0;
 	}
-	skeletonfile.close();
-	outfile.close();
-}
-	
-	/*простой последовательный поиск
-void Analyzator::analyze2() {
 
-	infile.open(in, std::ios_base::in);//режим in
-	if (!infile) {//открыть файл не удалось
-		std::cerr << "Ќе могу открыть\"" << in << "\"дл€ чтени€\n";
-		//exit( -1 );
-	}
-	State state = startHead;
-	char ch; int i = 0;
-	std::string buf;
-	while (infile.get(ch)) {
-		//infile.
-		//ищем начало
-		if (state == startHead && ch == startHeaderText[i]) {
-			if (i == startHeaderText.length() - 1) {
-				state = endHead;
-				i = 0;
-			}
-			else
-				i++;
-			continue;
-		}
-		//ищем конец  заголовка
-		if (state == endHead) {
-			buf += ch;
-			if (ch == endHeaderText[i] || (ch == endHeaderText[0] && (i = 0))) {
-				std::cout << "endHeaderText " << i << ch << endHeaderText[i] << std::endl;
-				if (i == endHeaderText.length() - 1) {
-
-					key = buf.substr(0, buf.length() - endHeaderText.length());
-					state = endBlock;
-					buf.clear();
-					i = 0;
-				}
-				else
-					i++;
-				continue;
-			}
-		}
-		//ищем конец  блока
-		if (state == endBlock) {
-			buf += ch;
-			if (ch == endBlockText[i] || (ch == endHeaderText[0] && (i = 0))) {
-				if (i == endBlockText.length() - 1) {
-					value = buf.substr(0, buf.length() - endBlockText.length());
-					std::cout << key << " | " << value << std::endl;
-
-					store.insert(std::pair<std::string, std::string>(key, value));
-					buf.clear();
-					state = startHead;
-					i = 0;
-				}
-				else
-					i++;
-				continue;
-			}
-		}
-
-		i = 0;
-	}
-	infile.close();
-}
-*/
-void Analyzator::error(char *str) {
-	std::cerr << str<<std::endl; 
+	return 0;
 }
