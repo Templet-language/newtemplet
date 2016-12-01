b1f8fb87be56ecd2f5e92a52d5e3fe9605363f1e
SergeyVostokin <easts@mail.ru>
2016-12-01 19:58:37 +0400

    some code added

--- gen/gen.cpp
+++ gen/gen.cpp
@@ -1,3 +1,11 @@
+﻿#include <string>
+#include <list>
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+/*
 #pragma templet ~message
 #pragma templet ~message =
 #pragma templet ~message@(submessage1?,submessage2!,submessage3!,-submessage4?)
@@ -8,12 +16,13 @@
 
 #pragma templet '~' id ['@'] [ '(' ['-'] id ('!'|'?') {',' ['-'] id ('!'|'?')} ')' ] ['=']
 #pragma templet '*' id ['@'] [ '(' ('?'| id ('!'|'?') id) {',' id ('!'|'?') id)} ')' ] ['+']
+*/
 
 struct submessage{
   string name;
   bool dummy;
   enum {CALL,ANSWER} type;
-}
+};
 
 struct message{
   string name;
@@ -36,25 +45,77 @@ struct actor{
   list<port> ports;
 };
 
-1) bool openparse(string&name, string&pragma);
+bool openparse(string&name,string&pragma);
 // открыть файл для разбора по имени,
 // pragma – обрабатывать только заданные прагмы, например, #pragma omp
-
-2) bool getpragma(string&argstring,int&line);
+bool getpragma(string&argstring,int&line);
 // argstring -- аргументы следующей по порядку разбора команды #pragma,
 // line – номер строки файла, с которой начинается прагма
-
-3) void closeparse();
+void closeparse();
 // закрытие файл
 
-1)    void lexinit(string&s);
+void lexinit(string&s);
 // инициализация; s – строка символов для разбора
-2)    bool getlex(string&lex);
+bool getlex(string&lex);
 // извлечение следующей лексемы из строки
 // return false – если лексем больше нет
-3)    bool ungetlex();
+bool ungetlex();
 // возврат на одну лексему назад
 // return false – если возврат невозможен (например, уже выполнен)
-4)    void lexclear();
+void lexclear();
 // очистка
 
+void error(int line)
+{
+	cerr << "error: bag #pragma at line " << line << endl;
+	exit(EXIT_FAILURE);
+}
+
+bool is_id(string&s)
+{
+	locale loc;
+	return s.length()>1 || (s.length()==1 && isalpha(s[0],loc));
+}
+
+bool parse_message(int line, message& m)
+{
+	string lex;
+	if (getlex(lex) && lex == "~"){
+		if (getlex(lex) && is_id(lex)){
+
+		}
+	}
+	else{
+		ungetlex();
+		return false;
+	}
+}
+
+bool parse_actor(int line, actor& a)
+{
+	string lex;
+	if (getlex(lex) && lex == "*"){
+		if (getlex(lex) && is_id(lex)){
+
+		}
+	}
+	else{
+		ungetlex();
+		return false;
+	}
+}
+
+void print_message(ostream& s, message& m)
+{
+
+}
+
+void print_actor(ostream&s,actor&a)
+{
+
+}
+
+int main(int argc, char *argv[])
+{
+	return EXIT_SUCCESS;
+}
