d4439c26316c3c0d2f36d9d75ba9e0a1dd2841e4
SergeyVostokin <easts@mail.ru>
2016-12-03 21:59:26 +0400

    build/print AST code added

--- gen/gen.cpp
+++ gen/gen.cpp
@@ -67,7 +67,7 @@ void lexclear();
 
 void error(int line)
 {
-	cerr << "error: bag #pragma at line " << line << endl;
+	cerr << "error: bad #pragma at line " << line << endl;
 	exit(EXIT_FAILURE);
 }
 
@@ -80,39 +80,183 @@ bool is_id(string&s)
 bool parse_message(int line, message& m)
 {
 	string lex;
-	if (getlex(lex) && lex == "~"){
-		if (getlex(lex) && is_id(lex)){
+	
+	if (!(getlex(lex) && lex == "~")){ ungetlex(); return false; }
 
+	if (!(getlex(lex) && is_id(lex))) error(line);
+
+	m.name = lex;
+	
+	if (getlex(lex) && lex == "@"){ m.serilizable = true; }
+	else{ ungetlex(); m.serilizable = false; }
+	
+	if (getlex(lex) && lex == "("){
+
+		submessage sm;
+
+		if (getlex(lex) && lex == "-"){ sm.dummy = true; }
+		else{ ungetlex(); sm.dummy = false; }
+		
+		if (!(getlex(lex) && is_id(lex))) error(line);
+
+		sm.name = lex;
+
+		if (getlex(lex) && lex == "?"){ sm.type = submessage::ANSWER; }
+		else if (ungetlex() && getlex(lex) && lex == "!"){ sm.type = submessage::CALL; }
+		else error(line);
+
+		m.subm.push_back(sm);
+
+		while (getlex(lex) && lex == ","){
+
+			if (getlex(lex) && lex == "-"){ sm.dummy = true; }
+			else{ ungetlex(); sm.dummy = false; }
+
+			if (!(getlex(lex) && is_id(lex))) error(line);
+
+			sm.name = lex;
+
+			if (getlex(lex) && lex == "?"){ sm.type = submessage::ANSWER; }
+			else if (ungetlex() && getlex(lex) && lex == "!"){ sm.type = submessage::CALL; }
+			else error(line);
+
+			m.subm.push_back(sm);
 		}
 	}
-	else{
-		ungetlex();
-		return false;
-	}
+	ungetlex();
+
+	if (!(getlex(lex) && lex == ")")) error(line);
+
+	if (getlex(lex) && lex == "="){ m.duplex = true; }
+	else{ ungetlex(); m.duplex = false; }
+
+	if (m.subm.size()) m.duplex = true;
+
+	if (getlex(lex)) error(line);
+
+	return true;
 }
 
 bool parse_actor(int line, actor& a)
 {
 	string lex;
-	if (getlex(lex) && lex == "*"){
-		if (getlex(lex) && is_id(lex)){
 
+	if (!(getlex(lex) && lex == "*")){ ungetlex(); return false; }
+
+	if (!(getlex(lex) && is_id(lex))) error(line);
+
+	a.name = lex;
+
+	if (getlex(lex) && lex == "@"){ a.serilizable = true; }
+	else{ ungetlex(); a.serilizable = false; }
+
+	if (getlex(lex) && lex == "("){
+
+		port p;
+
+		a.response_any = false;
+
+		if (getlex(lex) && lex == "?"){
+			a.response_any = true;
+		}
+		else{
+			ungetlex();
+
+			if (!(getlex(lex) && is_id(lex))) error(line);
+
+			p.name = lex;
+
+			if (getlex(lex) && lex == "?"){ p.type = port::SERVER; }
+			else if (ungetlex() && getlex(lex) && lex == "!"){ p.type = port::CLIENT; }
+			else error(line);
+
+			if (!(getlex(lex) && is_id(lex))) error(line);
+
+			p.message_name = lex;
+
+			a.ports.push_back(p);
+		}
+
+		while (getlex(lex) && lex == ","){
+
+			if (!(getlex(lex) && is_id(lex))) error(line);
+
+			p.name = lex;
+
+			if (getlex(lex) && lex == "?"){ p.type = port::SERVER; }
+			else if (ungetlex() && getlex(lex) && lex == "!"){ p.type = port::CLIENT; }
+			else error(line);
+
+			if (!(getlex(lex) && is_id(lex))) error(line);
+
+			p.message_name = lex;
+
+			a.ports.push_back(p);
 		}
 	}
-	else{
-		ungetlex();
-		return false;
-	}
+	ungetlex();
+
+	if (!(getlex(lex) && lex == ")")) error(line);
+
+	if (getlex(lex) && lex == "+"){ a.initially_active = true; }
+	else{ ungetlex(); a.initially_active = false; }
+
+	if (getlex(lex)) error(line);
+
+	return true;
 }
 
 void print_message(ostream& s, message& m)
 {
+	bool comma = false;
 
+	s << "~" << m.name;
+	if (m.serilizable) s << "@";
+
+	if (m.subm.size()){
+		s << "(";
+		for (auto x : m.subm){
+			if (comma) s << ",";
+
+			if (x.dummy) s << "-";
+			s << x.name;
+			if (x.type == submessage::ANSWER) s << "!";
+			else if (x.type == submessage::CALL) s << "?";
+			
+			comma = true;
+		}
+		s << ")";
+	}
+
+	if (m.duplex && m.subm.size() == 0) s << "=";
 }
 
 void print_actor(ostream&s,actor&a)
 {
+	bool comma = false;
 
+	s << "~" << a.name;
+	if (a.serilizable) s << "@";
+
+	if (a.ports.size()){
+		s << "(";
+
+		if (a.response_any){ s << "?"; comma = true; }
+
+		for (auto x : a.ports){
+			if (comma) s << ",";
+
+			s << x.name;
+			if (x.type == port::CLIENT) s << "!";
+			else if (x.type == port::SERVER) s << "?";
+			s << x.message_name;
+
+			comma = true;
+		}
+		s << ")";
+	}
+
+	if (a.initially_active) s << "+";
 }
 
 int main(int argc, char *argv[])
