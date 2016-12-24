f3c01ec2752d99abe0ffe42b6c3117cc2becb6e3
SergeyVostokin <easts@mail.ru>
2016-12-24 22:03:07 +0400

    start the pipeline sample

--- samples/pipeline/pipeline.cpp
+++ samples/pipeline/pipeline.cpp
@@ -17,139 +17,84 @@
 #include <iostream>
 #include <chrono>
 
-#define TET_DEBUG_SERIALIZATION
-#include <templet.hpp>
+// simulation of a calculation for an explicit FDM method using a pipeline
+// the FTCS method for the heat equation 
 
-// параллельное умножение матриц
-// с использованием портфеля задач
+const int N = 10;
+const int T = 10;
 
-const int P = 10; // число рабочих процессов (используется только для прогноза ускорения)
-const int N = 1000;
-double A[N][N], B[N][N], C[N][N];
+double Mp[N][N];
+double Ms[N][N];
 
 using namespace std;
 /*$TET$*/
 
-using namespace TEMPLET;
+struct pipeline{
+	pipeline(int num_stage, int num_iter)
+	{
+/*$TET$pipe$pipe*/
 
-struct task{
-	void save(saver*s){
-/*$TET$task$save*/
-		::save(s, &num, sizeof(num)); // строка num
-		::save(s, &A[num][0], sizeof(double)*N); // матрицы A
 /*$TET$*/
 	}
-	void restore(restorer*r){
-/*$TET$task$restore*/
-		::restore(r, &num, sizeof(num)); // строка num
-		::restore(r, &A[num][0], sizeof(double)*N); // матрицы A
-/*$TET$*/
-	}
-/*$TET$task$data*/
-	int num;// номер вычисляемой строки матрицы C
-/*$TET$*/
-};
 
-struct result{
-	void save(saver*s){
-/*$TET$result$save*/
-		::save(s, &num, sizeof(num)); // строка num 
-		::save(s, &C[num][0], sizeof(double)*N); // матрицы C
-/*$TET$*/
-	}
-	void restore(restorer*r){
-/*$TET$result$restore*/
-		::restore(r, &num, sizeof(num)); // строка num 
-		::restore(r, &C[num][0], sizeof(double)*N); // матрицы C
-/*$TET$*/
-	}
-/*$TET$result$data*/
-	int num; // номер вычисленной строки матрицы C
-/*$TET$*/
-};
-
-struct bag{
-	bag(int argc, char *argv[], int P=0);
 	void run();
 	void delay(double);
 	double speedup();
+	double speedup(int);
+	int numthreads();
 
-	bool get(task*t){
-/*$TET$bag$get*/
-		delay(0.10);
-		if (cur<N){ t->num = cur++; return true; }
-		else return false;
+	void stage(int num){
+/*$TET$pipe$stage*/
+
 /*$TET$*/
 	}
-	void put(result*r){
-/*$TET$bag$put*/
-// в этом примере не требует определения
-/*$TET$*/
-	}
-	void save(saver*s){
-/*$TET$bag$save*/
-		::save(s, &B[0][0], sizeof(double)*N*N); // матрица B
-/*$TET$*/
-	}
-	void restore(restorer*r){
-/*$TET$bag$restore*/
-		::restore(r, &B[0][0], sizeof(double)*N*N); // матрица B 
-/*$TET$*/
-	}
-/*$TET$bag$data*/
-	int cur; // курсор по строкам матрицы C
+/*$TET$pipe$data*/
+
 /*$TET$*/
 };
-
-void delay(double);
-
-void proc(task*t,result*r)
-{
-/*$TET$proc$data*/
-	delay(1.00);
-	int i = r->num = t->num;
-	for (int j = 0; j<N; j++){// параллельное вычисление строки матрицы C
-		C[i][j] = 0.0;
-		for (int k = 0; k<N; k++)C[i][j] += A[i][k] * B[k][j];
-	}
-/*$TET$*/
-}
 
 /*$TET$footer*/
 int main(int argc, char* argv[])
 {
-	bag b(argc, argv, P);
-	b.cur = 0; // начинаем вычисление с нулевой строки
+	double Tp, Ts;
 
-	// инициализация матриц
-	for (int i = 0; i<N; i++){
-		for (int j = 0; j<N; j++){
-			A[i][j] = 2.0; B[i][j] = 5.0; C[i][j] = 0.0;
-		}
-	}
+	pipeline p(N,T);
 
-	// параллельное умножение матриц c замером времени
+	std::cout << "the task dimentions: N (number of stages) = " << N
+		<< " T (number of data items passed thru) = " << T << endl;
+
 	auto start = std::chrono::high_resolution_clock::now();
-	b.run();
+	p.run();
 	auto end = std::chrono::high_resolution_clock::now();
 
-	std::cout << "multiplication time is "
-		<< (double)std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000000
-		<< " seconds\n";
+	if (p.speedup() <= 0.0){
+		Tp = ((double)std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000000);
+		std::cout << "the parallel computation time is " << Tp << " seconds" << endl;
 
-	// проверяем результат
+		start = std::chrono::high_resolution_clock::now();
+		// sequential code
+		end = std::chrono::high_resolution_clock::now();
+
+		Ts = ((double)std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000000);
+		std::cout << "the sequential computation time is " << Ts << " seconds" << endl;
+	}
+
 	for (int i = 0; i < N; i++){
 		for (int j = 0; j < N; j++){
-			if (C[i][j] != 10.0*N){
+			if (Mp[i][j] != Ms[i][j]){
 				printf("Something went wrong!!!\n");
 				return EXIT_FAILURE;
 			}
 		}
 	}
-	
-	// узнаём прогноз ускорения алгоритма (Smax), если накладные расходы составляют 10% от времени вычисления 
-	double Smax;
-	if ((Smax = b.speedup())>0.0) std::cout << "Smax = " << Smax << " (if use " << P << " workers)";
+
+
+	if (p.speedup()>0.0){
+		std::cout << "the maximum theoretical speedup is " << p.speedup() << endl;
+		std::cout << "the theoretical speedup for " << p.numthreads() << " is " << p.speedup(p.numthreads()) << endl;
+	}
+	else
+		std::cout << "the experimental speedup is " << Ts / Tp << "for " << p.numthreads() << "threads" << endl;
 
 	return EXIT_SUCCESS;
 }
