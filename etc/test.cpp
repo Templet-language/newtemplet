c3b3929646a35295ae3a83a1514477f762494590
Сергей Востокин <SergeyVostokin@users.noreply.github.com>
2019-04-26 08:16:13 +0300

    test.cpp -- new binding to messages

---
+++ etc/test.cpp
@@ -0,0 +1,35 @@
+﻿#include "pch.h"
+#include <iostream>
+
+struct A{};
+
+struct B{
+	void (A::*mref)();
+	A* oref;
+
+	template <class T>
+	void setup(void (T::*m)(), A*o) {
+		mref = reinterpret_cast<void(A::*)()>(m); oref = o;
+	}
+
+	void call() {
+		(oref->*mref)();
+	}
+};
+
+struct C:A{
+	void f(B&b) {
+		b.setup(&C::xxx,this);
+	}
+
+	void xxx() {
+		std::cout << "hello";
+	}
+};
+
+int main()
+{
+	C c; B b;
+	c.f(b);
+	b.call();
+}
