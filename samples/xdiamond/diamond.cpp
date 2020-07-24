ef84baf01f9fe4f1bd580d9cd8e00bc4845848d5
SergeyVostokin <easts@mail.ru>
2020-07-24 22:24:23 +0400

    xtemplet first iteration ready

--- samples/xdiamond/diamond.cpp
+++ samples/xdiamond/diamond.cpp
@@ -1,3 +1,19 @@
+/*--------------------------------------------------------------------------*/
+/*  Copyright 2020 Sergei Vostokin                                          */
+/*                                                                          */
+/*  Licensed under the Apache License, Version 2.0 (the "License");         */
+/*  you may not use this file except in compliance with the License.        */
+/*  You may obtain a copy of the License at                                 */
+/*                                                                          */
+/*  http://www.apache.org/licenses/LICENSE-2.0                              */
+/*                                                                          */
+/*  Unless required by applicable law or agreed to in writing, software     */
+/*  distributed under the License is distributed on an "AS IS" BASIS,       */
+/*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*/
+/*  See the License for the specific language governing permissions and     */
+/*  limitations under the License.                                          */
+/*--------------------------------------------------------------------------*/
+
 #include "diamond.hpp"
 #include <iostream>
 
@@ -7,9 +23,9 @@ int main()
 	base_engine teng;
 
 	A a(eng,teng);
-	B b(eng, teng);
-	C c(eng, teng);
-	D d(eng, teng);
+	B b(eng,teng);
+	C c(eng,teng);
+	D d(eng,teng);
 
 	b.a(a.b); c.a(a.c);
 	d.b(b.d); d.c(c.d);
